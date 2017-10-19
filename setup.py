6d99bcb16c4cdcf0853e9e84cf33c6526657e15a
jborbely <joe.borbely@gmail.com>
2017-10-20 11:54:31 +1300

    rewrite setup.py to not depend on any other files
    insert classes ApiDocs, BuildDocs
    delete files docs/docs_commands.py and requirements*.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,61 @@
-import os
 import re
 import sys
+from distutils.cmd import Command
 from setuptools import setup, find_packages
 
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'docs'))
-import docs_commands
+
+class ApiDocs(Command):
+    """
+    A custom command that calls sphinx-apidoc
+    see: http://www.sphinx-doc.org/en/latest/man/sphinx-apidoc.html
+    """
+    description = 'builds the api documentation using sphinx-apidoc'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from sphinx.apidoc import main
+        main([
+            'sphinx-apidoc',
+            '--force',  # overwrite existing files
+            '--module-first',  # put module documentation before submodule documentation
+            '--separate',  # put documentation for each module on its own page
+            '-o', './docs/_autosummary',  # where to save the output files
+            'msl',
+        ])
+        sys.exit(0)
+
+
+class BuildDocs(Command):
+    """
+    A custom command that calls sphinx-build
+    see: http://www.sphinx-doc.org/en/latest/man/sphinx-build.html
+    """
+    description = 'builds the documentation using sphinx-build'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from sphinx import build_main
+        build_main([
+            'sphinx-build',
+            '-b', 'html',  # the builder to use, e.g., create a HTML version of the documentation
+            '-a',  # generate output for all files
+            '-E',  # ignore cached files, forces to re-read all source files from disk
+            'docs',  # the source directory where the documentation files are located
+            './docs/_build/html',  # where to save the output files
+        ])
+        sys.exit(0)
 
 
 def read(filename):
@@ -33,8 +84,8 @@ setup(
     url='https://github.com/MSLNZ/msl-network',
     description='Asynchronous network I/O.',
     long_description=read('README.rst'),
-    license='MIT',
     platforms='any',
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -46,14 +97,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Physics',
-    ],  # list the classifiers, see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
     setup_requires=sphinx + pytest_runner,
     tests_require=['pytest-cov', 'pytest'],
-    install_requires=read('requirements.txt').splitlines() if not testing else [],
-    cmdclass={
-        'docs': docs_commands.BuildDocs,
-        'apidocs': docs_commands.ApiDocs,
-    },
+    install_requires=['cryptography'],
+    cmdclass={'docs': BuildDocs, 'apidocs': ApiDocs},
     entry_points={
         'console_scripts': [
             'msl-network = msl.network.cli:main',
@@ -61,4 +109,5 @@ setup(
     },
     packages=find_packages(include=('msl*',)),
     include_package_data=True,
+    zip_safe=False,
 )
