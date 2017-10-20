d681cac41bc1033b75a5240fcb19df5c10090091
jborbely <joe.borbely@gmail.com>
2017-10-20 18:21:45 +1300

    rename the input argument

--- msl/network/utils.py
+++ msl/network/utils.py
@@ -4,18 +4,18 @@ Common functions.
 import os
 
 
-def ensure_root_path(file_path):
+def ensure_root_path(path):
     """Ensure that the root directory of the file path exists.
         
     Parameters
     ----------
-    file_path : :obj:`str`
+    path : :obj:`str`
         The file path.
         
-        For example, if `file_path` is ``path/to/my/test/file.txt`` then 
-        this function would ensure that the ``path/to/my/test`` directory 
+        For example, if `path` is ``/the/path/to/my/test/file.txt`` then
+        this function would ensure that the ``/the/path/to/my/test`` directory
         exists creating the intermediate directories if necessary.
     """
-    root = os.path.dirname(file_path)
+    root = os.path.dirname(path)
     if root and not os.path.isdir(root):
         os.makedirs(root)
