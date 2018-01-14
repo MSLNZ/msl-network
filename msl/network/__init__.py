8f8dce59e1bc5b79c67819ecfbece4820a2a8457
jborbely <joe.borbely@gmail.com>
2018-01-15 10:55:10 +1300

    update copyright to 2018

--- msl/network/__init__.py
+++ msl/network/__init__.py
@@ -8,7 +8,7 @@ from .service import Service
 from .database import ConnectionsTable, HostnamesTable, UsersTable
 
 __author__ = 'Joseph Borbely'
-__copyright__ = '\xa9 2017, ' + __author__
+__copyright__ = '\xa9 2017 - 2018, ' + __author__
 __version__ = '0.2.0.dev0'
 
 version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')[:3]))
