6c94ab4d643ada09d220870893414e699b524eef
jborbely <joe.borbely@gmail.com>
2018-08-24 17:54:56 +1200

    add releaselevel to version_info

--- msl/network/__init__.py
+++ msl/network/__init__.py
@@ -1,6 +1,7 @@
 """
 Concurrent and asynchronous network I/O.
 """
+import re
 from collections import namedtuple
 
 from .client import connect
@@ -12,5 +13,7 @@ __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017 - 2018, ' + __author__
 __version__ = '0.2.0.dev0'
 
-version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')[:3]))
-""":obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro) tuple."""
+_v = re.search('(\d+).(\d+).(\d+)\.?(.*)', __version__).groups()
+
+version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
+""":obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
