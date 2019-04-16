2d2253267257da789c0a6dc7087b5b72c38fc53b
jborbely <joe.borbely@gmail.com>
2019-04-16 12:19:24 +1200

    bump v0.4.0 -> v0.4.1.dev0

--- msl/network/__init__.py
+++ msl/network/__init__.py
@@ -18,9 +18,9 @@ from .database import (
 
 __author__ = 'Joseph Borbely'
 __copyright__ = '\xa9 2017 - 2019, ' + __author__
-__version__ = '0.4.0'
+__version__ = '0.4.1.dev0'
 
 _v = re.search(r'(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
 
-version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), 'final')
+version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
