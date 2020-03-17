b711598769b5a3f8ef8dbce5cdcec43bb5798da5
jborbely <joe.borbely@gmail.com>
2020-03-18 12:50:20 +1300

    release v0.5.0

--- msl/network/__init__.py
+++ msl/network/__init__.py
@@ -26,9 +26,9 @@ from .manager import (
 
 __author__ = 'Measurement Standards Laboratory of New Zealand'
 __copyright__ = '\xa9 2017 - 2020, ' + __author__
-__version__ = '0.5.0.dev0'
+__version__ = '0.5.0'
 
 _v = re.search(r'(\d+)\.(\d+)\.(\d+)[.-]?(.*)', __version__).groups()
 
-version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), _v[3])
+version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(_v[0]), int(_v[1]), int(_v[2]), 'final')
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
