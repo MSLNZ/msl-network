9e1e9dfc4a6786ff4a481b550cb31211719189f8
jborbely <joe.borbely@gmail.com>
2020-09-10 12:05:44 +1200

    import Command from setuptools instead of distutils.cmd

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ import os
 import re
 import sys
 import subprocess
-from distutils.cmd import Command
-from setuptools import setup, find_packages
+from setuptools import (
+    setup,
+    find_packages,
+    Command,
+)
 
 if sys.version_info[:2] < (3, 5):
     sys.exit('Python < 3.5 is not supported because async/await syntax is required')
