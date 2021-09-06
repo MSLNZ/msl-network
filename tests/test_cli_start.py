794257b390b71159485e859c45994ca7da413983
jborbely <joe.borbely@gmail.com>
2021-09-07 10:05:35 +1200

    rewrite test_cli_start.py

--- tests/test_cli_start.py
+++ tests/test_cli_start.py
@@ -1,25 +1,11 @@
 import os
-import sys
-import tempfile
+
+import pytest
 
 import conftest
 
 from msl.network import cli
 from msl.network.database import UsersTable
-
-db = os.path.join(tempfile.gettempdir(), 'test_cli_start.db')
-out = os.path.join(tempfile.gettempdir(), 'test_cli_start.tmp')
-for item in [db, out]:
-    if os.path.isfile(item):
-        os.remove(item)
-
-table = UsersTable(database=db)
-
-
-def teardown_module(module):
-    table.close()
-    os.remove(db)
-    os.remove(out)
 
 
 def get_args(command):
@@ -27,34 +13,43 @@ def get_args(command):
     return parser.parse_args(command.split())
 
 
-def test_cannot_use_multiple_auth_methods():
-    flags = [
-        '--auth-hostname --auth-password hello',
-        '--auth-hostname --auth-login',
-        '--auth-login --auth-password hello world!',
-    ]
-    for flag in flags:
-        sys.stdout = open(out, 'w')
-        args = get_args('start ' + flag + ' --logfile ' + helper.ServiceStarter.logfile)
-        args.func(args)
-        sys.stdout.close()
-        lines = [line.strip() for line in open(out, 'r').readlines()]
-        assert lines[-1].endswith('ValueError: Cannot specify multiple authentication methods')
-
-
-def test_invalid_port():
-    sys.stdout = open(out, 'w')
-    args = get_args('start --port 1234x --logfile ' + helper.ServiceStarter.logfile)
+@pytest.mark.parametrize(
+    'flag',
+    ['--auth-hostname --auth-password hello',
+     '--auth-hostname --auth-login',
+     '--auth-login --auth-password h e l l o']
+)
+def test_cannot_use_multiple_auth_methods(flag, capsys):
+    args = get_args('start ' + flag)
     args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[-1].endswith('ValueError: The port number must be an integer')
+    _, err = capsys.readouterr()
+    assert err.rstrip().endswith('ValueError: Cannot specify multiple authentication methods')
 
 
-def test_cannot_use_auth_login_with_empty_table():
-    sys.stdout = open(out, 'w')
-    args = get_args('start --auth-login --database ' + db + ' --logfile ' + helper.ServiceStarter.logfile)
+@pytest.mark.parametrize('port', [-1, '1234x'])
+def test_invalid_port(port, capsys):
+    args = get_args('start --port {}'.format(port))
     args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[-2].endswith('ValueError: The Users table is empty. No one could log in.')
+    _, err = capsys.readouterr()
+    assert err.rstrip().endswith('ValueError: The port must be a positive integer')
+
+
+def test_cannot_use_auth_login_with_empty_table(capsys):
+    db = conftest.Manager.database
+    try:
+        os.remove(db)
+    except OSError:
+        pass
+
+    table = UsersTable(database=db)
+    args = get_args('start --auth-login --database ' + db)
+    args.func(args)
+    table.close()
+    out, err = capsys.readouterr()
+    out_lines = out.splitlines()
+    err_lines = err.splitlines()
+    for i in [1, 2, 3]:
+        assert db in os.path.normpath(out_lines[i])
+    assert len(err_lines) == 2
+    assert err_lines[0] == 'ValueError: The Users table is empty. No one could log in.'
+    os.remove(db)
