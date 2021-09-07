ad78b942ce53e17cd6ccc6594ebcdda9b40c7544
jborbely <joe.borbely@gmail.com>
2021-09-07 17:00:27 +1200

    rewrite test_cli_hostname.py

--- tests/test_cli_hostname.py
+++ tests/test_cli_hostname.py
@@ -1,107 +1,134 @@
 import os
-import sys
-import tempfile
+
+import pytest
+
+import conftest
 
 from msl.network import cli
 from msl.network.database import HostnamesTable
-
-db = os.path.join(tempfile.gettempdir(), 'test_cli_hostname.db')
-out = os.path.join(tempfile.gettempdir(), 'test_cli_hostname.tmp')
-table = HostnamesTable(database=db)
+from msl.network.constants import DATABASE
+from msl.network.utils import localhost_aliases
 
 
-def get_args(command):
+def process(command):
     parser = cli.configure_parser()
-    return parser.parse_args(command.split())
-
-
-def teardown_module(module):
-    table.close()
-    os.remove(db)
-    os.remove(out)
-
-
-def test_cli_hostname():
-    for h in ['hostname_'+str(i) for i in range(10)]:
-        table.insert(h)
-
-    original_names = table.hostnames()
-
-    base_command = 'hostname --database {} '.format(db)
-
-    # action: list
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'list')
+    args = parser.parse_args(command.split())
     args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('Trusted')
-    assert lines[2] == 'Hostnames:'
-    assert 'hostname_0' in lines
-    assert 'hostname_9' in lines
 
-    # action: add ... nothing
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'add')
-    args.func(args)
-    assert len(table.hostnames()) == len(original_names)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('No hostnames were')
 
-    # action: remove ... nothing
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'remove')
-    args.func(args)
-    assert len(table.hostnames()) == len(original_names)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('No hostnames were')
+def remove_default():
+    try:
+        os.remove(DATABASE)
+    except OSError:
+        pass
 
-    # action: add
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'add HOSTNAME1 HOSTNAME2')
-    args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('Added HOSTNAME1')
-    assert lines[1].startswith('Added HOSTNAME2')
+
+def test_path():
+    conftest.Manager.remove_files()
+    remove_default()
+
+    assert DATABASE != conftest.Manager.database
+    assert not os.path.isfile(DATABASE)
+    assert not os.path.isfile(conftest.Manager.database)
+
+    # need to specify an action, so use the "list" action
+    process('hostname list')
+    assert os.path.isfile(DATABASE)
+    assert not os.path.isfile(conftest.Manager.database)
+    os.remove(DATABASE)
+
+    process('hostname list --database {}'.format(conftest.Manager.database))
+    assert os.path.isfile(conftest.Manager.database)
+    assert not os.path.isfile(DATABASE)
+    os.remove(conftest.Manager.database)
+
+
+def test_list(capsys):
+    process('hostname list')
+    out, err = capsys.readouterr()
+    assert not err
+    out_lines = out.splitlines()
+    assert out_lines[0] == 'Trusted devices in {}'.format(DATABASE)
+    assert not out_lines[1]
+    assert out_lines[2] == 'Hostnames:'
+    for i, alias in enumerate(sorted(localhost_aliases())):
+        assert out_lines[i+3] == '  ' + alias
+
+
+@pytest.mark.parametrize(
+    ('action', 'verb'),
+    [('add', 'added'), ('insert', 'inserted'),
+     ('remove', 'removed'), ('delete', 'deleted')]
+)
+def test_no_hostnames(action, verb, capsys):
+    process('hostname ' + action)
+    out, err = capsys.readouterr()
+    assert out.rstrip() == 'No hostnames were {}'.format(verb)
+    assert not err
+
+
+@pytest.mark.parametrize(
+    ('action', 'verb'),
+    [('add', 'Added'), ('insert', 'Inserted')]
+)
+def test_add(action, verb, capsys):
+    remove_default()
+
+    process('hostname {} HOSTNAME1 abc123 MSLNZ-12345'.format(action))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out.splitlines() == [
+        '{} HOSTNAME1'.format(verb),
+        '{} abc123'.format(verb),
+        '{} MSLNZ-12345'.format(verb)
+    ]
+
+    table = HostnamesTable()
     assert 'HOSTNAME1' in table.hostnames()
-    assert 'HOSTNAME2' in table.hostnames()
+    assert 'abc123' in table.hostnames()
+    assert 'MSLNZ-12345' in table.hostnames()
+    table.close()
 
-    # action: remove
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'remove HOSTNAME1 HOSTNAME2')
-    args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('Removed HOSTNAME1')
-    assert lines[1].startswith('Removed HOSTNAME2')
-    assert 'HOSTNAME1' not in table.hostnames()
-    assert 'HOSTNAME2' not in table.hostnames()
+    remove_default()
 
-    # action: insert
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'insert ABC def XYZ')
-    args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('Inserted ABC')
-    assert lines[1].startswith('Inserted def')
-    assert lines[2].startswith('Inserted XYZ')
-    assert 'ABC' in table.hostnames()
-    assert 'def' in table.hostnames()
-    assert 'XYZ' in table.hostnames()
 
-    # action: delete
-    sys.stdout = open(out, 'w')
-    args = get_args(base_command + 'delete ABC def XYZ')
-    args.func(args)
-    sys.stdout.close()
-    lines = [line.strip() for line in open(out, 'r').readlines()]
-    assert lines[0].startswith('Deleted ABC')
-    assert lines[1].startswith('Deleted def')
-    assert lines[2].startswith('Deleted XYZ')
-    assert 'ABC' not in table.hostnames()
-    assert 'def' not in table.hostnames()
-    assert 'XYZ' not in table.hostnames()
+@pytest.mark.parametrize(
+    ('action', 'verb'),
+    [('remove', 'Removed'), ('delete', 'Deleted')]
+)
+def test_remove(action, verb, capsys):
+    remove_default()
+
+    process('hostname {} 127.0.0.1 ::1 localhost'.format(action))
+    out, err = capsys.readouterr()
+    assert not err
+    assert out.splitlines() == [
+        '{} 127.0.0.1'.format(verb),
+        '{} ::1'.format(verb),
+        '{} localhost'.format(verb)
+    ]
+
+    table = HostnamesTable()
+    assert 'localhost' not in table.hostnames()
+    assert '::1' not in table.hostnames()
+    assert '127.0.0.1' not in table.hostnames()
+    table.close()
+
+    remove_default()
+
+
+@pytest.mark.parametrize(
+    ('action', 'verb'),
+    [('remove', 'Removed'), ('delete', 'Deleted')]
+)
+def test_remove_invalid(action, verb, capsys):
+    process('hostname {} mslnz ::1 abc'.format(action))
+    out, err = capsys.readouterr()
+    out_lines = out.splitlines()
+    assert out_lines == [
+        "Cannot {} 'mslnz'. This hostname is not in the table.".format(action),
+        '{} ::1'.format(verb),
+        "Cannot {} 'abc'. This hostname is not in the table.".format(action),
+    ]
+    assert not err
+    remove_default()
