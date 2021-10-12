7710fc31617e5339aa52a7f63a7b9950274431e0
jborbely <joe.borbely@gmail.com>
2021-10-13 09:46:32 +1300

    loosen the assert requirements in test_emit_notification

--- tests/test_emit_notification.py
+++ tests/test_emit_notification.py
@@ -51,8 +51,8 @@ def test_client_linkedclient_handlers():
     lc_hb.notification_handler = handler2
     time.sleep(5)
 
-    assert len(values1) > 60
-    assert len(values2) > 30
+    assert len(values1) > 30
+    assert len(values1) > len(values2) * 1.5  # ideally len(values1) == len(values2) * 2
     assert len(values3) == 0  # the Echo Service does not emit notifications
     assert len(values4) == 0  # the Echo Service does not emit notifications
     assert values1.count(3) == 2  # the value 3 should appear twice since reset() was called twice
