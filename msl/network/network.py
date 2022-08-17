badcc43e643c88b883a7b8c300f2f3c05a5dd7d8
jborbely <joe.borbely@gmail.com>
2022-08-17 18:57:47 +1200

    call StreamWriter.wait_closed() when cleaning up
    This fixes the following warning
    
    sys:1: ResourceWarning: unclosed <socket.socket fd=2092, family=AddressFamily.AF_INET6, type=SocketKind.SOCK_STREAM, proto=0, laddr=('::1', 54126, 0, 0), raddr=('::1', 1875, 0, 0)>
    ResourceWarning: Enable tracemalloc to get the object allocation traceback
    ..\asyncio\proactor_events.py:115: ResourceWarning: unclosed transport <_ProactorSocketTransport fd=-1 read=<_OverlappedFuture cancelled>>
      _warn(f"unclosed transport {self!r}", ResourceWarning, source=self)
    ResourceWarning: Enable tracemalloc to get the object allocation traceback

--- msl/network/network.py
+++ msl/network/network.py
@@ -457,6 +457,15 @@ class Device(Network):
             self._reader.feed_eof()
             self._loop.run_until_complete(self._queue.join())
             self._writer.close()
+            try:
+                self._loop.run_until_complete(self._writer.wait_closed())
+            except AttributeError:
+                # TODO StreamWriter.wait_closed() was added in Python 3.7
+                #  This try-except block can be simplified when dropping
+                #  support for Python 3.6
+                async def wait_closed():
+                    await asyncio.sleep(0.01)
+                self._loop.run_until_complete(wait_closed())
             self._loop.close()
             try:
                 logger.info('disconnected from Manager[%s]', self._address_manager)
